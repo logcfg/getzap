ba6a5f4c423a7e81c2750f6bcb7b1f639827986f
System Warrior <62945336+WarSys@users.noreply.github.com>
2020-04-29 01:21:43 +0800

    First basic version (#2)
    * Refine doc.go
    
    * Add docs for new
    
    * complete GetDevelopmentLogger
    
    * Add GetProductionLogger
    
    * remove legacy set

---
+++ config.go
@@ -0,0 +1,116 @@
+package getzap
+
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	lj "gopkg.in/natefinch/lumberjack.v2"
+)
+
+// GetDevelopmentLogger returns a sophisticated, sophisticated logger for development.
+//
+// Logs with level below than ERROR will be written to:
+//    a) stdout in TSV format with colored level;
+//    b) normal log files in JSON format with rotation and expiration;
+//
+// Logs with level ERROR or above will be written to:
+//    a) stderr in TSV format with colored level;
+//    b) error log files in JSON format with rotation and expiration;
+//
+// Logs with level DPANIC or above will cause panic after writing the message.
+func GetDevelopmentLogger(normalLogPath, errorLogPath string) *zap.Logger {
+	var (
+		normalLogFile = zapcore.AddSync(&lj.Logger{
+			Filename:   normalLogPath,
+			MaxBackups: 20, // numbers
+			MaxSize:    10, // megabytes
+			MaxAge:     14, // days
+		})
+		errorLogFile = zapcore.AddSync(&lj.Logger{
+			Filename:   errorLogPath,
+			MaxBackups: 20, // numbers
+			MaxSize:    10, // megabytes
+			MaxAge:     14, // days
+		})
+	)
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(consoleEncoder, writeStdout, normalLevel),
+		zapcore.NewCore(jsonEncoder, normalLogFile, normalLevel),
+		zapcore.NewCore(consoleEncoder, writeStderr, errorLevel),
+		zapcore.NewCore(jsonEncoder, errorLogFile, normalLevel),
+	)
+	options := []zap.Option{
+		zap.Development(),
+		zap.AddCaller(),
+		zap.AddStacktrace(zap.ErrorLevel),
+	}
+	return zap.New(core, options...)
+}
+
+// GetProductionLogger returns a sophisticated, customized logger for production deployment.
+//
+// Logs with level below than INFO will be ignored.
+//
+// Logs with level INFO or above will be written to:
+//    a) stdout in JSON format;
+//    b) log files in JSON format with rotation, expiration and compression;
+//
+// Logs with level PANIC or above will cause panic after writing the message.
+func GetProductionLogger(logPath string) *zap.Logger {
+	var (
+		logFile = zapcore.AddSync(&lj.Logger{
+			Filename: logPath,
+			MaxSize:  10, // megabytes
+			MaxAge:   30, // days
+			Compress: true,
+		})
+	)
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(jsonEncoder, writeStdout, infoLevel),
+		zapcore.NewCore(jsonEncoder, logFile, infoLevel),
+	)
+	options := []zap.Option{
+		zap.AddCaller(),
+		zap.AddStacktrace(zap.ErrorLevel),
+	}
+	return zap.New(core, options...)
+}
+
+var (
+	genEncoderConfig = func(lvlEnc zapcore.LevelEncoder) *zapcore.EncoderConfig {
+		return &zapcore.EncoderConfig{
+			TimeKey:        "time",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			MessageKey:     "msg",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    lvlEnc,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.StringDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		}
+	}
+	// check if the log level is less than error
+	normalLevel = zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl < zapcore.ErrorLevel
+	})
+	// check if the level is greater than or equal to error
+	errorLevel = zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
+	})
+	// check if the level is greater than or equal to info
+	infoLevel = zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.InfoLevel
+	})
+	// log encoders
+	consoleEncoder = zapcore.NewConsoleEncoder(*genEncoderConfig(zapcore.CapitalColorLevelEncoder))
+	jsonEncoder    = zapcore.NewJSONEncoder(*genEncoderConfig(zapcore.LowercaseLevelEncoder))
+	// std log writers
+	writeStdout = zapcore.AddSync(os.Stdout)
+	writeStderr = zapcore.AddSync(os.Stderr)
+)
---
+++ doc.go
@@ -0,0 +1,15 @@
+/*
+Package getzap provides a collection of preset configurations for uber-go/zap logger.
+
+For each config, the following aspects are covered:
+
+	| Setting            | Option#1       | Option#2 |
+	| ------------------ | -------------- | -------- |
+	| Environment        | Dev            | Prod     |
+	| Format             | TSV with color | JSON     |
+	| Output Target      | Console        | File     |
+	| Separate Errors    | Yes            | No       |
+	| Remove Legacy Logs | Yes            | No       |
+
+*/
+package getzap
