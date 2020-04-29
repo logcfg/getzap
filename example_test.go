e1bdf84b19f975e60d0bc0205ef9ee89dfb9e6af
WarSys <warsys@pm.me>
2020-04-29 16:42:40 +0800

    Add example

--- example_test.go
+++ example_test.go
@@ -13,7 +13,8 @@ func ExampleGetDevelopmentLogger() {
 
 	logger.Debug("This is a DEBUG message")
 	logger.Info("This is an INFO message")
-	logger.Info("This is an INFO message with fields", zap.Int("id", 1), zap.Duration("sleep", 233*time.Millisecond))
+	logger.Info("This is an INFO message with fields", zap.Int("id", 1), zap.Duration("sleep", 128*time.Millisecond))
+	logger.Sugar().Infof("The answer to life the universe and everything = %d", 42)
 	logger.Warn("This is a WARN message")
 	logger.Error("This is an ERROR message")
 
@@ -26,7 +27,35 @@ func ExampleGetProductionLogger() {
 
 	logger.Debug("This is a DEBUG message")
 	logger.Info("This is an INFO message")
-	logger.Info("This is an INFO message with fields", zap.Int("id", 1), zap.Duration("sleep", 233*time.Millisecond))
+	logger.Info("This is an INFO message with fields", zap.Int("id", 1), zap.Duration("sleep", 64*time.Millisecond))
+	logger.Warn("This is a WARN message")
+	logger.Error("This is an ERROR message")
+	logger.DPanic("This is a DPANIC message")
+
+	// Output:
+}
+
+func ExampleSetGlobalDevelopmentLogger() {
+	getzap.SetGlobalDevelopmentLogger("", "log/error.log")
+	logger := zap.L().Sugar().Named("dev")
+
+	logger.Debug("This is a DEBUG message")
+	logger.Info("This is an INFO message")
+	logger.Infow("This is an INFO message with fields", "id", 1, "sleep", 256*time.Millisecond)
+	logger.Infof("The answer to life the universe and everything = %d", 42)
+	logger.Warn("This is a WARN message")
+	logger.Error("This is an ERROR message")
+
+	// Output:
+}
+
+func ExampleSetGlobalProductionLogger() {
+	getzap.SetGlobalProductionLogger("log/app.log")
+	logger := zap.L().Named("prod")
+
+	logger.Debug("This is a DEBUG message")
+	logger.Info("This is an INFO message")
+	logger.Info("This is an INFO message with fields", zap.Int("id", 1), zap.Duration("sleep", 32*time.Millisecond))
 	logger.Warn("This is a WARN message")
 	logger.Error("This is an ERROR message")
 	logger.DPanic("This is a DPANIC message")
