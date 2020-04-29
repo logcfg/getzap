615cf90fcdafc92ad92b65872d3d03f0db39cbe4
WarSys <warsys@pm.me>
2020-04-29 16:07:02 +0800

    Fix considered

--- doc.go
+++ doc.go
@@ -1,15 +1,14 @@
 /*
 Package getzap provides a collection of preset configurations for uber-go/zap logger.
 
-For each config, the following aspects are covered:
+For each config, the following aspects are considered:
 
-	| Setting            | Option#1       | Option#2 |
-	| ------------------ | -------------- | -------- |
-	| Environment        | Dev            | Prod     |
-	| Format             | TSV with color | JSON     |
-	| Output Target      | Console        | File     |
-	| Separate Errors    | Yes            | No       |
-	| Remove Legacy Logs | Yes            | No       |
-
+	| Setting              | Option #1      | Option #2 |
+	| -------------------- | -------------- | --------- |
+	| Environment          | Dev            | Prod      |
+	| Format               | TSV with color | JSON      |
+	| Output Target        | Console        | File      |
+	| Separate Errors      | Yes            | No        |
+	| Compress Legacy Logs | Yes            | No        |
 */
 package getzap
